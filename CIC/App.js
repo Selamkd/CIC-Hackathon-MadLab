3a52ef8e3ddeacb10eecf008d77d168b33aea945
Selam Ararsa <selamkedir@icloud.com>
2024-02-28 13:44:14 +0000

    background

--- CIC/App.js
+++ CIC/App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Image, Text, View, ActivityIndicator } from 'react-native';
+import { useState, useEffect } from 'react';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello world!</Text>
     </View>
   );
 }
@@ -13,7 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#51ad63',
     alignItems: 'center',
     justifyContent: 'center',
   },
