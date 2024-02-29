3c2e029bb9b84b060b97a5e8cb2de07ca9bafcf1
David Rozsavolgyi <muraminsav@gmail.com>
2024-02-29 10:51:48 +0000

    -

---
+++ utils/Questions.js
@@ -0,0 +1,33 @@
+const questions = [
+    { id: 1, text: 'What is your age?', type: 'text' },
+    { id: 2, text: 'What is your gender identity?', type: 'text' },
+    { id: 3, text: 'What is your ethnicity or race?', type: 'droplist',options:["Red","Green","Blue"] },
+    { id: 4, text: 'Where are you located?', type: 'text' },
+    {
+      id: 5,
+      text: 'What is your highest level of education completed?',
+      type: 'text',
+    },
+    { id: 6, text: 'What is your approximate household income?', type: 'text' },
+    {
+      id: 7,
+      text: 'What is the highest level of education and approximate income of your parents or guardians?',
+      type: 'text',
+    },
+    { id: 8, text: 'Are you currently employed?', type: 'text' },
+    { id: 9, text: 'What is the primary language spoken at home?', type: 'text' },
+    {
+      id: 10,
+      text: 'Do you have any accessibility needs we should be aware of?',
+      type: 'text',
+    },
+    { id: 11, text: 'What are your interests or hobbies?', type: 'text' },
+    {
+      id: 12,
+      text: 'Have you attended similar workshops or events before?',
+      type: 'text',
+    },
+    { id: 13, text: 'How did you find out about our workshops?', type: 'text' },
+  ];
+  
+  export default questions
