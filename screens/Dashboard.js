24aaa3d607adbcdfcf7ec94008e48aa90cd42257
Selam Ararsa <selamkedir@icloud.com>
2024-02-29 10:57:20 +0000

    background colors changed

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,36 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, Text, View, ActivityIndicator } from 'react-native';
 import { useState, useEffect } from 'react';
-import {getData,storeData} from '../utils/AsyncStorage';
+import { getData, storeData } from '../utils/AsyncStorage';
 
 import DashboardIcon from '../components/DashboadIcon';
 
-
 export default function Dashboard({ navigation }) {
-    const [listAllSessionForms,setListAllQuestionForms] = useState([])
-    const [ref, setRef]=useState(true)
+  const [listAllSessionForms, setListAllQuestionForms] = useState([]);
+  const [ref, setRef] = useState(true);
 
-    useEffect(() => {
-        console.log("lodingDB")
-        getData("sessionForms").then(data=>data? console.log(data," DASH") || setListAllQuestionForms(data): console.log(data, "falisy DB load")).catch(er=>console.log(er))
-        
-        
-    },[ref])
-    
+  useEffect(() => {
+    console.log('lodingDB');
+    getData('sessionForms')
+      .then((data) =>
+        data
+          ? console.log(data, ' DASH') || setListAllQuestionForms(data)
+          : console.log(data, 'falisy DB load')
+      )
+      .catch((er) => console.log(er));
+  }, [ref]);
+
   return (
     <View style={styles.container}>
       <View style={styles.iconList}>
-
-        <DashboardIcon navigation ={navigation} payload = {"question1"}
+        <DashboardIcon
+          navigation={navigation}
+          payload={'question1'}
           style={styles.iconShadow}
           title={'WorkShop 1'}
         ></DashboardIcon>
-        <DashboardIcon navigation ={navigation} payload = {"question2"} title={'WorkShop 2'}></DashboardIcon>
-        <DashboardIcon navigation ={navigation} payload = {"question3"} title={'WorkShop 3'}></DashboardIcon>
-        
+        <DashboardIcon
+          navigation={navigation}
+          payload={'question2'}
+          title={'WorkShop 2'}
+        ></DashboardIcon>
+        <DashboardIcon
+          navigation={navigation}
+          payload={'question3'}
+          title={'WorkShop 3'}
+        ></DashboardIcon>
       </View>
-        <Text onPress={()=>{ setRef(!ref);console.log(listAllSessionForms)}}>Consolelog</Text>
-
+      <Text
+        onPress={() => {
+          setRef(!ref);
+          console.log(listAllSessionForms);
+        }}
+      >
+        Consolelog
+      </Text>
     </View>
   );
 }
@@ -38,7 +55,7 @@ export default function Dashboard({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#51ad63',
+    backgroundColor: '#fffff',
     justifyContent: 'center',
   },
   iconList: {
